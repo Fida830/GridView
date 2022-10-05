9631fc4f63a57a8f7d173ba57f155684a86107d3
Fida Muhammad <muhammadfida830@gmail.com>
2022-10-05 15:48:59 +0500

    Initial Version

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,31 @@ void main(){
 
 class GridViewApp extends StatelessWidget{
 
+  List<Icon> myIcons = [
+    Icon(Icons.home,size: 70,),
+    Icon(Icons.contact_phone,size: 70,),
+    Icon(Icons.map,size: 70,),
+    Icon(Icons.phone,size: 70,),
+    Icon(Icons.camera,size: 70,),
+    Icon(Icons.settings,size: 70,),
+    Icon(Icons.album,size: 70,),
+    Icon(Icons.wifi,size: 70,),
+
+  ];
+
+  List<Text> myTexts = [
+    Text("Home",style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),),
+    Text("Contact",style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),),
+    Text("Map",style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),),
+    Text("Phone",style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),),
+    Text("Camera",style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),),
+    Text("Settings",style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),),
+    Text("Album",style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),),
+    Text("Wifi",style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),)
+  ];
+
+
+
 
   Widget build(BuildContext context){
     return MaterialApp(
@@ -24,194 +49,43 @@ class GridViewApp extends StatelessWidget{
             crossAxisCount: 3,
             crossAxisSpacing: 10,
             mainAxisSpacing: 10,
-            children: [
-              Container(
-                height: 100,
-                width: 100,
-                decoration: BoxDecoration(
-                  color: Colors.amber,
-                ),
 
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: List.generate(myIcons.length, (index){
+              return _createWidget(myIcons[index],myTexts[index]);
 
+            }),
 
-                  children: [
-                    Icon(Icons.home,size: 70,),
-                    Text("Home",style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),)
-                  ],
-                ),
-              ),
-
-              Container(
-                height: 100,
-                width: 100,
-                decoration: BoxDecoration(
-                  color: Colors.amber,
-
-
-                ),
-
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-
-
-                  children: [
-                    Icon(Icons.contact_phone,size: 70,),
-                    Text("Contact",style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),)
-                  ],
-                )
-                ,
-
-
-              ),
-
-              Container(
-                height: 100,
-                width: 100,
-                decoration: BoxDecoration(
-                  color: Colors.amber,
-                ),
-
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-
-
-                  children: [
-                    Icon(Icons.map,size: 70,),
-                    Text("Map",style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),)
-                  ],
-                ),
-              ),
-              Container(
-                height: 100,
-                width: 100,
-                decoration: BoxDecoration(
-                  color: Colors.amber,
-
-
-                ),
-
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-
-
-                  children: [
-                    Icon(Icons.phone,size: 70,),
-                    Text("Phone",style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),)
-                  ],
-                ),
-              ),
-
-              Container(
-                height: 100,
-                width: 100,
-                decoration: BoxDecoration(
-                  color: Colors.amber,
-
-
-                ),
-
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-
-
-                  children: [
-                    Icon(Icons.camera,size: 70,),
-                    Text("Camera",style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),)
-                  ],
-                ),
-
-              ),
-
-              Container(
-                height: 100,
-                width: 100,
-                decoration: BoxDecoration(
-                  color: Colors.amber,
-
-
-                ),
-
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-
-
-                  children: [
-                    Icon(Icons.settings,size: 70,),
-                    Text("Settings",style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),)
-                  ],
-                ),
-
-              ),
-
-              Container(
-                height: 100,
-                width: 100,
-                decoration: BoxDecoration(
-                  color: Colors.amber,
-
-
-                ),
-
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-
-
-                  children: [
-                    Icon(Icons.album,size: 70,),
-                    Text("Album",style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),)
-                  ],
-                ),
-
-              ),
-
-              Container(
-                height: 100,
-                width: 100,
-                decoration: BoxDecoration(
-                  color: Colors.amber,
-
-
-                ),
-
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-
-
-                  children: [
-                    Icon(Icons.wifi,size: 70,),
-                    Text("Wifi",style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),)
-                  ],
-                ),
-
-              ),
-
-
-
-
-            ],
           ),
         ),
       ),
 
     );
+  }
+
+
+
+  Widget _createWidget(Icon icon, Text text){
+
+    return   Container(
+      height: 100,
+      width: 100,
+      decoration: BoxDecoration(
+        color: Colors.amber,
+
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          icon,
+          text
+        ],
+      )
+    );
+
+
+
   }
 
 }
